652b1133d13e54c4e275f7a4aeed75460de9f8d0
Romain Maurer <romain.maurer@gmail.com>
2019-10-14 21:49:26 +1100

    First app

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Transactions from './routes/Transactions.svelte'
+	import Budgets from './routes/Budgets.svelte'
+	import Reports from './routes/Reports.svelte'
+	import Router from 'svelte-spa-router'
+	import Nav from './components/Nav.svelte'
+
+	export let name;
+	export let segment;
+
+	const routes = {
+    '/': Transactions,
+    '/transactions': Transactions,
+	'/budgets': Budgets,
+	'/reports': Reports,
+	'*': Transactions
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+	div#app {
+		background-color: white;
+        margin-left: auto;
+        margin-right: auto;
+        width: 1024px;
+	}
+</style>
+
+<div id="app">
+
+<Nav {segment}/>
+<Router {routes}/>
+
+</div>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,53 @@
+<script>
+    import {link, location} from 'svelte-spa-router'
+</script>
+
+<style>
+	nav {
+		border-bottom: 1px solid rgba(255,62,0,0.1);
+		font-weight: 300;
+		padding: 0 1em;
+	}
+	ul {
+		margin: 0;
+		padding: 0;
+	}
+	/* clearfix */
+	ul::after {
+		content: '';
+		display: block;
+		clear: both;
+	}
+	li {
+		display: block;
+		float: left;
+	}
+	.selected {
+		position: relative;
+		display: inline-block;
+	}
+	.selected::after {
+		position: absolute;
+		content: '';
+		width: calc(100% - 1em);
+		height: 2px;
+		background-color: rgb(255,62,0);
+		display: block;
+		bottom: -1px;
+	}
+	a {
+		text-decoration: none;
+		padding: 1em 0.5em;
+		display: block;
+        color: rgb(220,62,0);
+        font-size: 1.2em;
+	}
+</style>
+
+<nav>
+	<ul>
+		<li><a class='{$location === "/" ? "selected" : ""}' href='/' use:link>transactions</a></li>
+		<li><a class='{$location === "/budgets" ? "selected" : ""}' href='/budgets' use:link>budgets</a></li>
+		<li><a class='{$location === "/reports" ? "selected" : ""}' href='/reports' use:link>reports</a></li>
+	</ul>
+</nav>
